a276a403695dd4b8e3a7e0758234bbb643051db3
Lauri Litovuo <llitovuo@student.hive.fi>
2024-04-01 21:04:00 +0300

    headerfile with needed libraries init

---
+++ incl/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/01 20:59:19 by llitovuo          #+#    #+#             */
+/*   Updated: 2024/04/01 21:03:21 by llitovuo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <unistd.h> //if write
+
+#endif
