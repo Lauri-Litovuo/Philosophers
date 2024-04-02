0036013cb359a743b317b78d7c6e92f0de4a892c
Lauri Litovuo <llitovuo@student.hive.fi>
2024-04-02 14:53:32 +0300

    adding enum

--- incl/philo.h
+++ incl/philo.h
@@ -6,18 +6,26 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 20:59:19 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/04/01 21:03:21 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/04/02 14:52:50 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
-
 # include <string.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/time.h>
 # include <pthread.h>
 # include <unistd.h> //if write
+
+typedef enum e_state
+{
+	EAT = 1,
+	SLEEP = 2,
+	THINK = 3
+}	t_state;
+
+
 
 #endif
