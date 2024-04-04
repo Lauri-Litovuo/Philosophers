802cc51b0d77f290c0daa5fbfeb1c9b91c19a6c9
Lauri Litovuo <llitovuo@student.hive.fi>
2024-04-04 14:13:17 +0300

    adding error msg function

---
+++ srcs/errors.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 12:26:11 by llitovuo          #+#    #+#             */
+/*   Updated: 2024/04/04 13:54:48 by llitovuo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incl/philo.h"
+
+void	input_error_msg(int errcode)
+{
+	if (errcode == NOTDIGIT)
+	{
+		write(2, "Only positive numbers are allowed as arguments.\n", 48);
+	}
+	if (errcode == ZERO)
+	{
+		write(2, "Number's value must be bigger than zero.\n", 41);
+	}
+	if (errcode == COUNT)
+	{
+		write(2, "Invalid number of arguments (4-5 required)\n", 44);
+	}
+}
