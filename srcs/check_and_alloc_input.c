4b02f30475b442f06a8b49fa191a96d7a3e7fbc5
Lauri Litovuo <llitovuo@student.hive.fi>
2024-04-04 14:12:06 +0300

    fixing existing functions and adding new ones to check proper input

--- srcs/check_and_alloc_input.c
+++ srcs/check_and_alloc_input.c
@@ -6,29 +6,106 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 09:56:16 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/04/04 10:08:28 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/04/04 14:07:47 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/philo.h"
 
-t_data	*validate_and_alloc_input(int ac, char **av)
-{
-	t_data	*data;
+static int	check_for_digits(int ac, char **av);
+static int	args_to_ints(char *arg);
+static int	ft_strlen(char *str);
+static int	ft_isdigit(int c);
 
-	if (ac != 5 || ac != 6) //check
+int	*validate_and_alloc_input(int ac, char **av)
+{
+	int	*data;
+	int	i;
+
+	data = NULL;
+	if (check_for_digits(ac, av) != 0)
+	{
+		input_error_msg(NOTDIGIT);
 		return (NULL);
-	if (check_for_digit(ac, av) != 0)
-		return (NULL);
-	data = init_data();
+	}
+	data = (int *) malloc (ac * sizeof(int));
 	if (!data)
 		return (NULL);
-	if (ac == 6) //check
-		data->flag = 1;
-	data->philo_count = philo_count(av[1]);
-	data->sleep_time = get_sleep_time(av[2]);
-	data->eating_time = get_eating_time(av[3]);
-	if (data->flag == 1)
-		data->eat_count = get_eat_count(av[4]);
+	i = 0;
+	while (i < ac)
+	{
+		data[i] = args_to_ints(av[i + 1]);
+		if (data[i] == 0)
+		{
+			input_error_msg(ZERO);
+			return (free(data), NULL);
+		}
+		i++;
+	}
+	data[i] = 0;
 	return (data);
 }
+
+
+static int	check_for_digits(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i < ac - 1)
+	{
+		j = 0;
+		while (j < (int)ft_strlen(av[i + 1]))
+		{
+			if (ft_isdigit(av[i + 1][j]) == 0)
+				return (NOTDIGIT);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+static int	args_to_ints(char *arg)
+{
+	long long int	num;
+
+	num = 0;
+	if (!arg || arg[0] == '\0')
+		return (-1);
+	while (*arg != '\0')
+	{
+		if (*arg >= '0' && *arg <= '9')
+		{
+			num = num * 10 + *arg - '0';
+		}
+		else
+			return (-1);
+		arg++;
+	}
+	return ((int)num);
+}
+
+static int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+static int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+
+
